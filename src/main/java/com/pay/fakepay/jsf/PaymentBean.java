c0f8b87e17b7dc8c00dc65e260c9848e6db23cea
ben <ben.pearce@sky.com>
2021-03-09 22:59:19 +0000

    Add PaymentBean as request scoped java bean

---
+++ src/main/java/com/pay/fakepay/jsf/PaymentBean.java
@@ -0,0 +1,47 @@
+package com.pay.fakepay.jsf;
+
+import com.pay.fakepay.ejb.TransactionService;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+
+@Named
+@RequestScoped public class PaymentBean {
+    
+    @EJB
+    TransactionService transactionService;
+    
+    String username;
+    
+    float amount;
+    
+    public PaymentBean() { }
+    
+    public String pay() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        
+        String user = request.getRemoteUser();
+        
+        transactionService.makePayment(user, username, amount);
+        return "user";
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+}
