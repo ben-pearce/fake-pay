e066dce727edd70a6029dd900be97a01ccd83d83
ben <ben.pearce@sky.com>
2021-03-09 22:59:49 +0000

    Add TransactionService EJB

---
+++ src/main/java/com/pay/fakepay/ejb/TransactionService.java
@@ -0,0 +1,41 @@
+package com.pay.fakepay.ejb;
+
+import com.pay.fakepay.entity.MoneyTransaction;
+import com.pay.fakepay.entity.SystemUser;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+
+@Stateless public class TransactionService {
+    
+    @PersistenceContext
+    EntityManager em;
+    
+    public TransactionService() { }
+    
+    public void makePayment(String from, String to, float amount) {
+        Query query = em.createNamedQuery("SystemUser.getUser");
+        query.setParameter("username", from);
+        
+        SystemUser sender = (SystemUser) query.getSingleResult();
+        
+        query = em.createNamedQuery("SystemUser.getUser");
+        query.setParameter("username", to);
+        
+        SystemUser recipient = (SystemUser) query.getSingleResult();
+        
+        MoneyTransaction transaction = new MoneyTransaction(
+                sender, 
+                recipient, 
+                amount, 
+                false);
+        
+        sender.setBalance(sender.getBalance() - amount);
+        recipient.setBalance(recipient.getBalance() + amount);
+        em.persist(sender);
+        em.persist(recipient);
+        em.persist(transaction);
+    }
+}
