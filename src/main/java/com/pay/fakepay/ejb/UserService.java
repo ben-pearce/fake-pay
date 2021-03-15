45a454bcba37729d2b2ff19c0fb35f065956af4b
ben <ben.pearce@sky.com>
2021-03-15 15:24:32 +0000

    Perform appropriate currency conversion on user initial balance

--- src/main/java/com/pay/fakepay/ejb/UserService.java
+++ src/main/java/com/pay/fakepay/ejb/UserService.java
@@ -1,13 +1,12 @@
 package com.pay.fakepay.ejb;
 
 import com.pay.fakepay.Currency;
-import com.pay.fakepay.entity.MoneyTransaction;
+import com.pay.fakepay.CurrencyExchange;
 import com.pay.fakepay.entity.SystemUser;
 import com.pay.fakepay.entity.SystemUserGroup;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
@@ -34,28 +33,40 @@ import javax.persistence.Query;
             SystemUserGroup sysUserGroup;
             
             MessageDigest md = MessageDigest.getInstance("SHA-256");
-            String passwd = userpassword;
-            md.update(passwd.getBytes("UTF-8"));
+            md.update(userpassword.getBytes("UTF-8"));
             byte[] digest = md.digest();
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < digest.length; i++) {
-                sb.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
+                sb.append(Integer
+                        .toString((digest[i] & 0xff) + 0x100, 16)
+                        .substring(1));
             }
-            String password = sb.toString();
+            String passwordHash = sb.toString();
             Currency userCurrency = Currency.valueOf(currency);
+            
+            Float balance = 1000f;
+            if(userCurrency != Currency.GBP) {
+                balance = CurrencyExchange.convert(
+                    Currency.GBP, 
+                    userCurrency, 
+                    balance);
+            }
+
             sysUser = new SystemUser(
                     username, 
-                    password, 
+                    passwordHash, 
                     name, 
                     surname,
-                    userCurrency
+                    userCurrency,
+                    balance
             );
             sysUserGroup = new SystemUserGroup(username, "users");
 
             em.persist(sysUser);
             em.persist(sysUserGroup);
         } catch (UnsupportedEncodingException | NoSuchAlgorithmException ex) {
-            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UserService.class.getName())
+                    .log(Level.SEVERE, null, ex);
         }
     }
     
