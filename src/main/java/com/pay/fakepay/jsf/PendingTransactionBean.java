2f8e0d81ee1b098e2470250b29e6618a1b625de8
ben <ben.pearce@sky.com>
2021-03-15 11:32:02 +0000

    Add pending transaction backing bean

---
+++ src/main/java/com/pay/fakepay/jsf/PendingTransactionBean.java
@@ -0,0 +1,34 @@
+package com.pay.fakepay.jsf;
+
+import com.pay.fakepay.ejb.TransactionService;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped public class PendingTransactionBean implements Serializable {
+    
+    @EJB
+    TransactionService transactionService;
+    
+    Long transactionId;
+
+    public PendingTransactionBean() { }
+    
+    public void acceptPendingTransaction() {
+        transactionService.acceptPendingTransaction(transactionId);
+    }
+    
+    public void declinePendingTransaction() {
+        transactionService.declinePendingTransaction(transactionId);
+    }
+    
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+}
